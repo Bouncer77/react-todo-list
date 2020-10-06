331e4fd2ed7a11dc2b8ae4a56dc21fee83025fab
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-10-06 11:54:12 +0300

    React компоненты

--- src/index.js
+++ src/index.js
@@ -6,15 +6,35 @@ import ReactDOM from 'react-dom';
 // const el = <h1>Hello, World!</h1>; // JSX код
 // ReactDOM.render(el, document.getElementById('root'));
 
-const el = (
-    <div>
+const AppHeader = () => {
+    return (
         <h1>My Todo List</h1>
+    );
+};
+
+const SearchPanel = () => {
+    return (
         <input placeholder="search" />
+    );
+};
+
+const TodoList = () => {
+    return (
         <ul>
             <li>Learn React</li>
             <li>Build Awesome App</li>
         </ul>
-    </div>
-);
+    );
+};
 
-ReactDOM.render(el, document.getElementById('root'));
+const App = () => {
+  return (
+      <div>
+          <AppHeader />
+          <SearchPanel />
+          <TodoList />
+      </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
