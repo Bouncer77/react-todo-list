1c1f125d3aa0f231bf4856e8cf746ee7aadb4d10
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-10-06 11:44:16 +0300

    React элементы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello, World!');
+import React from "react";
+import ReactDOM from 'react-dom';
+
+
+// const el = React.createElement('h1', null, 'Hello, World!');
+// const el = <h1>Hello, World!</h1>; // JSX код
+// ReactDOM.render(el, document.getElementById('root'));
+
+const el = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    </div>
+);
+
+ReactDOM.render(el, document.getElementById('root'));
