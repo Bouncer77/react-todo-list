5ab6f9185e1a82b4496c6565db58aac8078f7513
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-10-14 12:23:30 +0300

    Добавлен поиск элемента на странице

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './search-panel.css';
 
-const SearchPanel = () => {
-  return (
-    <input type="text"
-              className="form-control search-input"
-              placeholder="type to search" />
-  );
-};
+export default class SearchPanel extends Component {
 
-export default SearchPanel;
+    state = {
+        term: ''
+    };
+
+    onSearchChancge = (event) => {
+        const term = event.target.value;
+        this.setState({term});
+        this.props.onSearchChancge(term);
+    }
+
+    render() {
+        return (
+            <input type="text"
+                   className="form-control search-input"
+                   placeholder="type to search"
+                   value={this.state.term}
+                   onChange={this.onSearchChancge}/>
+        );
+    }
+}
